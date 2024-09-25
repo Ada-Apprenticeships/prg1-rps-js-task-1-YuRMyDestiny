aca0681f360d0452c08948bba77f6ae73830be2e
YuRMyDestiny <manchun.yu@ada.ac.uk>
2024-09-25 11:22:40 +0000

    remove testing console logs

--- rps.js
+++ rps.js
@@ -1,37 +1,25 @@
 function rockPaperScissors(player1, player2) {
-
   const WINCOMBS = {
-    "rock": ["scissors", "lizard"],
-    "paper": ["rock", "spock"],
-    "scissors": ["paper", "lizard"],
-    "lizard": ["spock", "paper"],
-    "spock": ["scissors", "rock"],
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"],
   };
 
-  if (player1 === player2){
+  if (player1 === player2) {
     return "draw";
   } else {
-    for (let choice of Object.keys(WINCOMBS)){
-      if (player1 === choice && WINCOMBS[choice].includes(player2)){
+    for (let choice of Object.keys(WINCOMBS)) {
+      if (player1 === choice && WINCOMBS[choice].includes(player2)) {
         return "player1";
       }
     }
     return "player2";
   }
 }
-   
-
-
-console.log(rockPaperScissors("scissors", "spock"));
-
-
-
-
-
-
-
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+};
