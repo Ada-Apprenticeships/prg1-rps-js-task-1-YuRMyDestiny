9c6cbe8cf180838fb188d0569a7205f65f9eb4cf
YuRMyDestiny <manchun.yu@ada.ac.uk>
2024-09-25 14:27:49 +0000

    Remove unnecessary loop

--- rps.js
+++ rps.js
@@ -10,12 +10,7 @@ function rockPaperScissors(player1, player2) {
   if (player1 === player2) {
     return "draw";
   }
-  for (const choice in WINCOMBS) {
-    if (player1 === choice && WINCOMBS[choice].includes(player2)) {
-      return "player1";
-    }
-  }
-  return "player2";
+  return WINCOMBS[player1].includes(player2) ? "player1" : "player2";
 }
 
 // Leave this code here for the automated tests
