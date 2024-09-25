233b3bf5ae778a85c56cfa9575df3c1ad356e81f
YuRMyDestiny <manchun.yu@ada.ac.uk>
2024-09-25 14:31:25 +0000

    Use proper case

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 function rockPaperScissors(player1, player2) {
-  const WINCOMBS = {
+  const winCombs = {
     rock: ["scissors", "lizard"],
     paper: ["rock", "spock"],
     scissors: ["paper", "lizard"],
@@ -10,7 +10,7 @@ function rockPaperScissors(player1, player2) {
   if (player1 === player2) {
     return "draw";
   }
-  return WINCOMBS[player1].includes(player2) ? "player1" : "player2";
+  return winCombs[player1].includes(player2) ? "player1" : "player2";
 }
 
 // Leave this code here for the automated tests
