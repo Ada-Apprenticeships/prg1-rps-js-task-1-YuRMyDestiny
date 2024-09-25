50d93203aef7afb2fd7fe2c6d04bfdfe3557cf04
YuRMyDestiny <manchun.yu@ada.ac.uk>
2024-09-25 10:24:18 +0000

    Implement basic rps logic

--- rps.js
+++ rps.js
@@ -1,12 +1,28 @@
 function rockPaperScissors(player1, player2) {
 
-    if (player1 === player2){
-      return "draw";
+  const WINCOMBS = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper",
+    "lizard": "spock",
+    "spock": "scissors",
+  };
+
+  if (player1 === player2){
+    return "draw";
+  } else {
+    for (let choice of Object.keys(WINCOMBS)){
+      if (player1 === choice && player2 === WINCOMBS[choice]){
+        return "player1";
+      }
     }
-   
+    return "player2";
+  }
 }
+   
 
 
+console.log(rockPaperScissors("scissors", "rock"));
 
 
 
